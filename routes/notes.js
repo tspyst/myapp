458e637d55333d4dc742db172376104676b281a5
tspy <2101110212cd@cyber-u.ac.jp>
2023-11-18 19:50:14 +0900

    add rest api service

---
+++ routes/notes.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+// レスポンスのデータ(ノート全体)
+const responseObjectDataAll = {
+textObject1 : {
+id: 1,
+title:'ノート1のタイトルです',
+subTitle:'ノート1のサブタイトルです',
+bodyText:'ノート1の本文です'	
+}	,
+textObject2 : {
+id: 2,
+title:'ノート2のタイトルです',
+subTitle:'ノート2のサブタイトルです',
+bodyText:'ノート2の本文です'	
+}	,
+};
+
+/**
+ * @returns{Object[]}data
+ * @returns{number}data.id - ID
+ * @returns{string}data.title - タイトル
+ * @returns{string}data.text - 内容
+ */
+router.get('/',function(req,res,next){
+//全件取得して返す
+res.json(responseObjectDataAll)	;
+})
+
+module.exports = router;
